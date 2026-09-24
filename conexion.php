331e917322cb0749f33ec2110dcf4ae84876975f
Suzzet868 <023101005b@uandina.edu.pe>
2026-09-24 08:19:43 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";      // Por defecto en XAMPP viene vacío
-$database = "revista_digital";   // Nombre de tu base de datos en MySQL
+$host = "sql308.infinityfree.com";
+$user = "if0_42999897";
+$password = "Solo456yo123"; // Escribe aquí la contraseña de tu cuenta de InfinityFree
+$database = "if0_42999897_revista_digital";
 
 // Crear la conexión
 $conexion = mysqli_connect($host, $user, $password, $database);
@@ -12,6 +12,6 @@ if (!$conexion) {
     die("Error al conectar con la base de datos: " . mysqli_connect_error());
 }
 
-// Configurar el conjunto de caracteres a UTF-8 (para evitar problemas con tildes y eñes)
-mysqli_set_charset($conexion, "utf8");
+// Configurar el conjunto de caracteres a UTF-8
+mysqli_set_charset($conexion, "utf8mb4");
 ?>
